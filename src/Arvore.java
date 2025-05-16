3f0356bfdd37d7b7e8ff33d4fbb3688f60f8c548
Malv741 <malvcronosgmail.com>
2025-05-15 23:33:55 -0300

    Adiciona Metodo mostrar árvore e corrige metodo inserir Filho

--- src/Arvore.java
+++ src/Arvore.java
@@ -6,16 +6,67 @@ public class Arvore {
         raiz = new No(conteudo);
     }
 
-    public void inserirFilho(String conteudo){
-        No atual = raiz;
-        if(raiz.getFilhoEsquerdo() == null || raiz.getFilhoDireito() == null){
+    public void inserirFilho(String conteudo) {
+
+        if (raiz.getFilhoEsquerdo() == null) {
             raiz.setFilhoEsquerdo(criarFilho(conteudo));
-        } else if (raiz.getFilhoEsquerdo() != null || raiz.getFilhoDireito() == null){
+            return;
+        } else if (raiz.getFilhoDireito() == null) {
             raiz.setFilhoDireito(criarFilho(conteudo));
+            return;
+        }
+
+        No atual = raiz.getFilhoEsquerdo();
+        No irmaoAtual = raiz.getFilhoDireito();
+
+        while (atual != null || irmaoAtual != null) {
+            if (atual != null) {
+                if (atual.getFilhoEsquerdo() == null) {
+                    atual.setFilhoEsquerdo(criarFilho(conteudo));
+                    return;
+                } else if (atual.getFilhoDireito() == null) {
+                    atual.setFilhoDireito(criarFilho(conteudo));
+                    return;
+                }
+            }
+
+            if (irmaoAtual != null) {
+                if (irmaoAtual.getFilhoEsquerdo() == null) {
+                    irmaoAtual.setFilhoEsquerdo(criarFilho(conteudo));
+                    return;
+                } else if (irmaoAtual.getFilhoDireito() == null) {
+                    irmaoAtual.setFilhoDireito(criarFilho(conteudo));
+                    return;
+                }
+            }
+
+            atual = atual != null ? atual.getFilhoEsquerdo() : null;
+            irmaoAtual = irmaoAtual != null ? irmaoAtual.getFilhoEsquerdo() : null;
         }
     }
 
     public No criarFilho(String conteudo){
         return new No(conteudo);
     }
+
+    public void mostrarArvore() {
+        mostrarArvore(raiz, 0);
+    }
+
+    private void mostrarArvore(No no, int espaco) {
+        if (no == null) {
+            return;
+        }
+
+        int incremento = 5;
+        mostrarArvore(no.getFilhoDireito(), espaco + incremento);
+
+        for (int i = 0; i < espaco; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(no.getConteudo());
+
+        mostrarArvore(no.getFilhoEsquerdo(), espaco + incremento);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Arvore arvore = new Arvore("A");
+        arvore.inserirFilho("B");
+        arvore.inserirFilho("C");
+        arvore.inserirFilho("D");
+        arvore.inserirFilho("E");
+        arvore.inserirFilho("F");
+
+        System.out.println("Atenção a árvore está deitada!!");
+        arvore.mostrarArvore();
     }
 }
